9027b6a55b74506d6232403555a166eb4b4e2d92
Boiko Ihor <ihor.boiko@appflame.com>
2023-07-20 16:41:01 +0300

    language migration

---
+++ database/migrations/2023_07_20_080804_language.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 2)->unique();
+            $table->unsignedInteger('created_at');
+            $table->string('name');
+            $table->boolean('is_active')->default(false);
+            $table->unsignedInteger('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('languages');
+    }
+};
