a4ddb818763180ae0853d48d9ca47da047e2d858
Boiko Ihor <ihor.boiko@appflame.com>
2023-07-24 16:15:22 +0300

    1205123602996591 admin middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ *
+ * @property int id
+ * @property string name
+ * @property string email
+ * @property string email_verified_at
+ * @property string password
+ * @property int role
+ * @property string remember_token
+ * @property string created_at
+ * @property string updated_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public const ROLE_ADMIN = 9;
 
     /**
      * The attributes that are mass assignable.
@@ -20,9 +35,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
+        'name',
         'password',
+        'role',
     ];
 
     /**
@@ -44,4 +60,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * @return int
+     */
+    public function getRole(): int
+    {
+        return $this->role;
+    }
 }
